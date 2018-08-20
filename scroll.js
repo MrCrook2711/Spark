01ad47263fea4061177b0976c111096be9cbc858
MrCrook2711 <39854068+MrCrook2711@users.noreply.github.com>
2018-08-20 18:28:00 +0530

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,17 +1,7 @@
 $(document).ready(function(){
-  $(".knowz").click(function(){
-      document.querySelector('#navbar').scrollIntoView({ 
+  $(".image").click(function(){
+      document.querySelector('.second').scrollIntoView({ 
         behavior: 'smooth' 
     });
   });
 }); 
-var offsetHeight = document.getElementById('first').offsetHeight;
-window.onscroll = genericFunction();
-var scroll = window.scrollTop();
-function genericFunction(){
-  if (scroll>offsetHeight){
-    $("#navbar").css("background","black");
-  }else{
-    $("#navbar").css("background","#2e3842");
-  } 
-}
