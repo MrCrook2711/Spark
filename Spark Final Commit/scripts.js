35cc488fc083cf6310b6c0086871778df60a1549
Nirbhay Singh <39854068+NirbhaySingh2711@users.noreply.github.com>
2018-09-01 10:53:36 +0530

    About and Team pages will be updated later.
    These are the final pages. Disregard all previous pages.

---
+++ Spark Final Commit/scripts.js
@@ -0,0 +1,38 @@
+$(window).scroll(function() {
+  var value = $(window).scrollTop()
+  if (value === 880){
+    $("#navbar").addClass("navbar-static")
+    $("#navbar").addClass("navbar-static")
+  }
+  $('#navbar').css({
+    opacity: function() {
+      console.log((value/1000))
+      return (value)/1000
+    }
+  });
+});
+$(document).ready(function(){
+    var width=$(window).width();
+    if (width<=768){
+      var autoHeight=$(".second").outerHeight();
+      if (width<=320){
+        var adjustedHeight = autoHeight+500;
+        $(".second").height(adjustedHeight);
+      }else if (width>=330){
+        var adjustedHeight = autoHeight+300;
+        $(".second").height(adjustedHeight);
+      $("li").removeClass("mt-0");
+      $("li").addClass("mt-2");
+      }
+    }
+   
+});
+  $("#toggler").click(function(){
+    $("#navbar").css("opacity","1.0");
+    $("#navbar").css("z-index","10");
+  });
+  $(".landing-image").click(function(){
+  document.querySelector('.second').scrollIntoView({
+        behavior: 'smooth'
+    });
+  });
